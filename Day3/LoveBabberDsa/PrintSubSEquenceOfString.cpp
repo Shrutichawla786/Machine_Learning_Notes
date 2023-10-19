ba5d32c02b52cb3af179506caf201e60edcf5e8b
shrutichawla786 <shruti.ch786gmail.com>
2023-10-19 16:21:34 +0530

    LoveBabber

---
+++ Day3/LoveBabberDsa/PrintSubSEquenceOfString.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+void SubSequence(string str ,int i , string output){
+    //basecase
+    if(i>=str.length()){
+        cout<<output<<endl;
+        return;
+    }
+    
+    //exclude
+    
+    SubSequence(str, i+1 , output);
+    
+    //include
+    output.push_back(str[i]);
+    SubSequence(str ,i+1 , output);
+    
+    
+    
+}
+
+
+
+
+int main()
+{
+    string str="abc";
+    int i=0;
+    string output="";
+    SubSequence(str , i , output);
+    
+
+    return 0;
+}
